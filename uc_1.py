912730be1bd49be4155bf3d75c511d7f6a4ebf3c
Lokesh <lokeshkumark.2145@gmail.com>
2024-12-16 11:44:10 +0530

    uc 1 solved

---
+++ uc_1.py
@@ -0,0 +1,10 @@
+import random
+
+print("------------player 1 vs player 2--------------")
+player1=input("player 1 : Name ------>")
+player2=input("player 2 : Name ------>")
+print(f"------------{player1} vs {player2}--------------")
+print(f"------------{player1} starts the game--------------")
+num=random.randint(0,1)
+
+        
