e06632042c572c95276f1b5a95a83c9a192e1d8b
Lokesh <lokeshkumark.2145@gmail.com>
2024-12-16 11:47:10 +0530

    uc 4 solved

---
+++ uc_4.py
@@ -0,0 +1,22 @@
+def game_play(x):
+    pos=0
+    result=0
+    flag=True
+    while flag:
+        if result<100:
+            result=generate_play(switch(),pos)
+            # result=98
+            if result<100:
+                pos=result
+        elif result==100:
+            print(result)
+            pos=0
+            flag=False
+        else:
+            if result>100:
+                # print(result)
+                result=pos
+                # print(pos)
+                continue
+    player=x
+    return f"{x} won the game"
